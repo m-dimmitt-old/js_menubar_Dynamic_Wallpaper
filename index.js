6a7f60783efd7416fcb7a9f3da2f586aa29c2c4a
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-01 20:54:16 -0400

    menubar: functionality to open files.

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 const {ipcRenderer} = require('electron')
 
+var opener = require("opener");
+
+const AerialDesktop = path.join(__dirname, 'Aerial_Desktop');
+const hiding = path.join(AerialDesktop, '.hide');
+const binning = path.join(hiding, 'bin');
+
+      function FractalClock() {
+        opener([path.join(binning, "Installer.app"), "--args", "FractalClock"]);
+      }
+
+      function MarineAquarium() {
+        opener([path.join(binning, "Installer.app"), "--args", "MarineAquarium3.2"]);
+      }
+
+      function StarWarsScroll() {
+        opener([path.join(binning, "Installer.app"), "--args", "StarWarsScroll"]);
+      }
+
+      function WebViewScreenSaver() {
+        opener([path.join(binning, "Installer.app"), "--args", "WebViewScreenSaver"]);
+      }
+
+      function MatrixScreenSaver() {
+        opener([path.join(binning, "Installer.app"), "--args", "MatrixScreenSaver"]);
+      }
+
+      function KPSaver() {
+        opener([path.join(binning, "Installer.app"), "--args", "KPSaver"]);
+      }
+
+      function BlueScreenSaver() {
+        opener([path.join(binning, "Installer.app"), "--args", "BlueScreenSaver"]);
+      }
+
+      function Aerial() {
+        opener([path.join(binning, "Installer.app"), "--args", "Aerial"]);
+      }
+
+      function Uninstall() {
+        opener (path.join(binning,'Uninstall.app'));
+      }
