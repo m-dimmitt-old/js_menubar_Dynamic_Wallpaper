2dbfe4d8645fff9b53050e5afee9d359cfc35feb
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-01 21:08:24 -0400

    fix file mapping.

--- Aerial_Desktop/.hide/app/install/1_check_resource.sh
+++ Aerial_Desktop/.hide/app/install/1_check_resource.sh
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   if [ -d ~/Library/Screen\ Savers/$screensaver ] || [ -d /Library/Screen\ Savers/$screensaver ] ; then
     echo "File exists."
   else
-    open $DIR/../bin/$screensaver/
+    open $DIR/../../bin/$screensaver/
   fi
