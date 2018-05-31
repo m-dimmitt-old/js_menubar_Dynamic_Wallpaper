050e90dfa0f2d1542357e411887641e0d507d107
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-05-31 17:29:46 -0400

    implementation searching macintosh operating system

--- Aerial_Desktop/.hide/app/install/4_desktop_background_changer.sh
+++ Aerial_Desktop/.hide/app/install/4_desktop_background_changer.sh
@@ -2,6 +2,7 @@
 
 # gather information to be able to run scripts from this file location.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $(./0_check_version.sh $(sw_vers -productVersion) 10.12.6 '<=')
 # say $DIR
 # end gather information.
 
